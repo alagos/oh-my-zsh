154c9155732d47b77bbbf58cd406a13feb5d3649
Alter Lagos <alagos@users.noreply.github.com>
2018-09-20 08:29:50 -0300

    Some more aliases

--- plugins/capistrano/capistrano.plugin.zsh
+++ plugins/capistrano/capistrano.plugin.zsh
@@ -1,5 +1,9 @@
 # Added `shipit` because `cap` is a reserved word. `cap` completion doesn't work.
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Modules.html#The-zsh_002fcap-Module
+alias cpd='cap production deploy'
+alias cpsr='cap production sidekiq:restart'
+alias cpsta='cap production sidekiq:start'
+alias cpsto='cap production sidekiq:stop'
 
 func capit() {
   if [ -f Gemfile ]
